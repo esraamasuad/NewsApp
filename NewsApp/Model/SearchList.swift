30beb877ce60b3357026baf33bd75d78b41335ac
esraa <esraa.masuad@gmail.com>
2023-12-09 13:00:35 +0200

    Start => Setup Offline Mode by [Realm] to cache the search result locally

---
+++ NewsApp/Model/SearchList.swift
@@ -0,0 +1,24 @@
+//
+//  SearchList.swift
+//  NewsApp
+//
+//  Created by Esraa on 08/12/2023.
+//
+
+import Foundation
+import RealmSwift
+
+class SearchList: Object, ObjectKeyIdentifiable {
+    @Persisted var query: String
+    @Persisted var date: String
+    @Persisted var articles: List<ArticleModel> = List<ArticleModel>()
+    
+    convenience init (query: String, articles: [ArticleModel]) {
+        self.init()
+        self.query = query
+        let _ = articles.map {
+            self.articles.append($0)
+        }
+    }
+}
+
