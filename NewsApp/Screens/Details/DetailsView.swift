c95f1b034b794a713e1ffc4fe0368ac7c0965005
esraa <esraa.masuad@gmail.com>
2023-12-09 13:06:32 +0200

    Resume => News Details enhance UI

--- NewsApp/Screens/Details/DetailsView.swift
+++ NewsApp/Screens/Details/DetailsView.swift
@@ -15,38 +15,34 @@ struct DetailsView: View {
     
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading, spacing: 20, content: {
-                Text(article.title ?? "")
-                    .foregroundColor(.black)
-                    .font(.largeTitle)
-                    .lineLimit(nil)
-                KFImage(URL(string: (article.urlToImage ?? "")))
-                    .resizable()
-                    .frame(height: 250)
-                    .cornerRadius(20)
-                Text(article.author ?? "")
-                    .foregroundColor(.gray)
-                Text(article.source?.name ?? "")
-                    .foregroundColor(.gray)
-                Button(action: {
-                    openURL(URL(string: article.url ?? "")!)
-                }, label: {
-                    VStack(alignment: .leading, spacing: 20, content: {
-                        Text(article.description ?? "")
-                            .foregroundColor(.black)
-                            .font(.caption)
-                        Text(article.content ?? "")
-                            .font(.body)
-                            .foregroundColor(.black)
-                    })
+            Link(destination: URL(string: article.url ?? "")!) {
+                VStack(alignment: .leading, spacing: 20, content: {
+                    Text(article.title ?? "")
+                        .foregroundColor(.black)
+                        .font(.largeTitle)
+                        .lineLimit(nil)
+                    KFImage(URL(string: (article.urlToImage ?? "")))
+                        .resizable()
+                        .frame(height: 250)
+                        .cornerRadius(20)
+                    Text(article.author ?? "")
+                        .foregroundColor(.gray)
+                    Text(article.source?.name ?? "")
+                        .foregroundColor(.gray)
+                    Text(article.description_ ?? "")
+                        .lineLimit(nil)
+                        .foregroundColor(.black)
+                        .font(.caption)
+                    Text(article.content ?? "")
+                        .lineLimit(nil)
+                        .font(.body)
+                        .foregroundColor(.black)
+                    Button("Read more...") {
+                        openURL(URL(string: article.url ?? "")!)
+                    }
                 })
-                Button("Read more...") {
-                    openURL(URL(string: article.url ?? "")!)
-                }
-                
-                //                Link("Read more...", destination: URL(string: article.url ?? "")!)
-            })
-            .padding()
+                .padding()
+            }
         }
     }
 }
