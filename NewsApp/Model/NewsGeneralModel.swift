6554ad56e9282b06f6545478b7e580cb8be8e593
esraa <esraa.masuad@gmail.com>
2023-12-09 13:05:46 +0200

    Change -> edit [ArticleModel] for realm usage

---
+++ NewsApp/Model/NewsGeneralModel.swift
@@ -0,0 +1,55 @@
+//
+//  NewsGeneralModel.swift
+//  NewsApp
+//
+//  Created by Esraa on 06/12/2023.
+//
+
+import Foundation
+import RealmSwift
+
+// MARK: - GeneralModel
+class NewsGeneralModel: Codable {
+    var status: String?
+    var totalResults: Int?
+    var articles: [ArticleModel]?
+    
+    init() {}
+    
+    init(articles: [ArticleModel]?) {
+        self.articles = articles
+    }
+}
+
+// MARK: - ArticleModel
+class ArticleModel: Object, ObjectKeyIdentifiable, Codable {
+    @Persisted var source: SourceModel?
+    @Persisted var author: String?
+    @Persisted var title: String?
+    @Persisted var description_: String?
+    @Persisted var url: String?
+    @Persisted var urlToImage: String?
+    @Persisted var publishedAt: String?
+    @Persisted var content: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case source = "source"
+        case author = "author"
+        case title = "title"
+        case description_ = "description"
+        case url = "url"
+        case urlToImage = "urlToImage"
+        case publishedAt = "publishedAt"
+        case content = "content"
+    }
+}
+
+// MARK: - SourceModel
+class SourceModel: Object, ObjectKeyIdentifiable, Codable {
+    @Persisted var id: String?
+    @Persisted var name: String?
+}
+
+enum MyError: Error {
+    case runtimeError(String)
+}
